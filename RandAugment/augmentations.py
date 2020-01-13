8ab84e413c797f27d37c35c74f3a635ff3907d3a
curtis.abcd <curtis.abcd@kakaobrain.com>
2020-01-13 11:24:49 +0900

    #9 : fix augmentation search space

--- RandAugment/augmentations.py
+++ RandAugment/augmentations.py
@@ -5,6 +5,7 @@ import random
 import PIL, PIL.ImageOps, PIL.ImageEnhance, PIL.ImageDraw
 import numpy as np
 import torch
+from PIL import Image
 
 
 def ShearX(img, v):  # [-0.3, 0.3]
@@ -29,6 +30,13 @@ def TranslateX(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
     return img.transform(img.size, PIL.Image.AFFINE, (1, 0, v, 0, 1, 0))
 
 
+def TranslateXabs(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
+    assert 0 <= v
+    if random.random() > 0.5:
+        v = -v
+    return img.transform(img.size, PIL.Image.AFFINE, (1, 0, v, 0, 1, 0))
+
+
 def TranslateY(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
     assert -0.45 <= v <= 0.45
     if random.random() > 0.5:
@@ -37,15 +45,8 @@ def TranslateY(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
     return img.transform(img.size, PIL.Image.AFFINE, (1, 0, 0, 0, 1, v))
 
 
-def TranslateXAbs(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
-    assert 0 <= v <= 10
-    if random.random() > 0.5:
-        v = -v
-    return img.transform(img.size, PIL.Image.AFFINE, (1, 0, v, 0, 1, 0))
-
-
-def TranslateYAbs(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
-    assert 0 <= v <= 10
+def TranslateYabs(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
+    assert 0 <= v
     if random.random() > 0.5:
         v = -v
     return img.transform(img.size, PIL.Image.AFFINE, (1, 0, 0, 0, 1, v))
@@ -79,14 +80,16 @@ def Solarize(img, v):  # [0, 256]
     return PIL.ImageOps.solarize(img, v)
 
 
+def SolarizeAdd(img, addition=0, threshold=128):
+    img_np = np.array(img).astype(np.int)
+    img_np = img_np + addition
+    img_np = np.clip(img_np, 0, 255)
+    img_np = img_np.astype(np.uint8)
+    img = Image.fromarray(img_np)
+    return PIL.ImageOps.solarize(img, threshold)
+
+
 def Posterize(img, v):  # [4, 8]
-    assert 4 <= v <= 8
-    v = int(v)
-    return PIL.ImageOps.posterize(img, v)
-
-
-def Posterize2(img, v):  # [0, 4]
-    assert 0 <= v <= 4
     v = int(v)
     return PIL.ImageOps.posterize(img, v)
 
@@ -156,25 +159,46 @@ def Identity(img, v):
 
 def augment_list():  # 16 oeprations and their ranges
     # https://github.com/google-research/uda/blob/master/image/randaugment/policies.py#L57
+    # l = [
+    #     (Identity, 0., 1.0),
+    #     (ShearX, 0., 0.3),  # 0
+    #     (ShearY, 0., 0.3),  # 1
+    #     (TranslateX, 0., 0.33),  # 2
+    #     (TranslateY, 0., 0.33),  # 3
+    #     (Rotate, 0, 30),  # 4
+    #     (AutoContrast, 0, 1),  # 5
+    #     (Invert, 0, 1),  # 6
+    #     (Equalize, 0, 1),  # 7
+    #     (Solarize, 0, 110),  # 8
+    #     (Posterize, 4, 8),  # 9
+    #     # (Contrast, 0.1, 1.9),  # 10
+    #     (Color, 0.1, 1.9),  # 11
+    #     (Brightness, 0.1, 1.9),  # 12
+    #     (Sharpness, 0.1, 1.9),  # 13
+    #     # (Cutout, 0, 0.2),  # 14
+    #     # (SamplePairing(imgs), 0, 0.4),  # 15
+    # ]
+
+    # https://github.com/tensorflow/tpu/blob/8462d083dd89489a79e3200bcc8d4063bf362186/models/official/efficientnet/autoaugment.py#L505
     l = [
-        (Identity, 0., 1.0),
-        (ShearX, 0., 0.3),  # 0
-        (ShearY, 0., 0.3),  # 1
-        (TranslateX, 0., 0.33),  # 2
-        (TranslateY, 0., 0.33),  # 3
-        (Rotate, 0, 30),  # 4
-        (AutoContrast, 0, 1),  # 5
-        (Invert, 0, 1),  # 6
-        (Equalize, 0, 1),  # 7
-        (Solarize, 0, 110),  # 8
-        (Posterize, 4, 8),  # 9
-        # (Contrast, 0.1, 1.9),  # 10
-        (Color, 0.1, 1.9),  # 11
-        (Brightness, 0.1, 1.9),  # 12
-        (Sharpness, 0.1, 1.9),  # 13
-        # (Cutout, 0, 0.2),  # 14
-        # (SamplePairing(imgs), 0, 0.4),  # 15
+        (AutoContrast, 0, 1),
+        (Equalize, 0, 1),
+        (Invert, 0, 1),
+        (Rotate, 0, 30),
+        (Posterize, 0, 4),
+        (Solarize, 0, 256),
+        (SolarizeAdd, 0, 110),
+        (Color, 0.1, 1.9),
+        (Contrast, 0.1, 1.9),
+        (Brightness, 0.1, 1.9),
+        (Sharpness, 0.1, 1.9),
+        (ShearX, 0., 0.3),
+        (ShearY, 0., 0.3),
+        (CutoutAbs, 0, 40),
+        (TranslateXabs, 0., 100),
+        (TranslateYabs, 0., 100),
     ]
+
     return l
 
 
